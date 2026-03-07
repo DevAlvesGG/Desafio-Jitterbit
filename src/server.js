8fae4aa240d8989fa4b3e539f077aecb90c5bc9f
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-03-07 20:04:16 -0300

    feat: create config for file to routes

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
+const router = require('./routes');
 
 //configuracao para utilizar Json
 app.use(express.json());
 
+//consfiguracao de rota
+app.use(router);
+
 const PORT = process.env.PORT || 3006;
 
-app.get('/', (req, res) => {
-  res.json('Hello, World!');
-});
 
 app.listen(PORT, () => console.log(`Server is running in http://localhost:${PORT}`));
